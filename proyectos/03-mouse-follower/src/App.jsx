9d044990ad80f4fc09cef456d3d9f62c3d0fa0aa
edwar.munoz <edwarmunoz.turri@gmail.com>
2024-06-11 09:00:49 -0500

    Proyecto 3

---
+++ proyectos/03-mouse-follower/src/App.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react"
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0});
+  
+  useEffect(() => {
+    console.log('efecto', {enabled})
+  
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY })
+    }
+  
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove);
+    }
+  
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+  
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </>
+  )
+}
+
+function App() {
+  return(
+    <main>
+      <FollowMouse />
+    </main>
+  )
+}
+
+export default App
